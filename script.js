f997f3ad905d831c49306be7e0c552097f167444
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-08-22 13:16:05 +0300

    basics on html and css

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [
+  {
+    author: "George RR Martin",
+    title: "A Song of Ice and Fire",
+    pages: 1299,
+    read: true,
+  },
+  {
+    author: "Ga",
+    title: "As Fire",
+    pages: 129,
+    read: true,
+  },
+];
+
+function Book(author, title, pages, readStatus) {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
